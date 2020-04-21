242b6a6947404f95cced9a29f8e90a403d1200a7
ShawnDa <shawnda761@gmail.com>
2020-04-22 11:59:04 +1200

    Add a new method about double calculation

--- HelloMaven/src/main/java/com/adventureIsland/maven/helloMaven/HelloMaven.java
+++ HelloMaven/src/main/java/com/adventureIsland/maven/helloMaven/HelloMaven.java
@@ -14,11 +14,20 @@ public class HelloMaven {
 		}
 		return result;
 	}
+	
+	public double calculate(double... pars) {
+		double result = 0.00;
+		for (double par : pars) {
+			result += par;
+		}
+		return result;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(new HelloMaven().responseToDefaultOutput("Hello Maven"));
 		System.out.println(new HelloMaven().calculate(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+		System.out.println(new HelloMaven().calculate(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0));
 	}
 
 }
