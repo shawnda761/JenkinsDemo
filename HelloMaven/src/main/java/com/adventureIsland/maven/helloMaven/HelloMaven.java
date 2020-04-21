a1ec44fc8b9670d73e41718aed8d77e3e8d010b0
ShawnDa <shawnda761@gmail.com>
2020-04-22 11:42:09 +1200

    Add a new function about integer calculation.

--- HelloMaven/src/main/java/com/adventureIsland/maven/helloMaven/HelloMaven.java
+++ HelloMaven/src/main/java/com/adventureIsland/maven/helloMaven/HelloMaven.java
@@ -4,13 +4,21 @@ public class HelloMaven {
 
 	public String responseToDefaultOutput(String input) {
 		String result = "The Input is: " + input;
-		System.out.println(result);
+		return result;
+	}
+	
+	public int calculate(int... pars) {
+		int result = 0;
+		for (int par : pars) {
+			result += par;
+		}
 		return result;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(new HelloMaven().responseToDefaultOutput("Hello Maven"));
+		System.out.println(new HelloMaven().calculate(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 	}
 
 }
