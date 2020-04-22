ceb56b19773b462caa11f8f3317211d006679443
ShawnDa <shawnda761@gmail.com>
2020-04-22 13:59:16 +1200

    Add a multiply method.

--- HelloMaven/src/main/java/com/adventureIsland/maven/helloMaven/HelloMaven.java
+++ HelloMaven/src/main/java/com/adventureIsland/maven/helloMaven/HelloMaven.java
@@ -22,11 +22,20 @@ public class HelloMaven {
 		}
 		return result;
 	}
+	
+	public int multiply(int... pars) {
+		int result = 1;
+		for (int par : pars) {
+			result *= par;
+		}
+		return result;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(new HelloMaven().responseToDefaultOutput("Hello Maven"));
 		System.out.println(new HelloMaven().calculate(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+		System.out.println(new HelloMaven().multiply(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 		System.out.println(new HelloMaven().calculate(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0));
 	}
 
